ce9985ec28f890625e3015dd465a2d7cba4cab81
Jeet Kumar <happyboy8117@gmail.com>
2026-08-09 15:51:41 +0530

    Checks driving eligibility

---
+++ C-plus/driving eligibility.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+int main(){
+    int age;
+    cout<<"Enter your age : "<<;
+    cin>>age;
+    if(age>=18) cout<<"ELIGIBLE"<<endl;
+    else cout<<"NOT ELIGIBLE"<<endl;
+}
