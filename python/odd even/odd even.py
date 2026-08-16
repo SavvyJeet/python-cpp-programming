be7c678f5ecf03e1d65cd3edaf9a35b8b586e8d5
Jeet Kumar <happyboy8117@gmail.com>
2026-08-16 14:33:42 +0530

    Enhanced the code

--- python/odd even/odd even.py
+++ python/odd even/odd even.py
@@ -1,5 +1,8 @@
-num = int(input("Enter a number: "))
-if num % 2 == 0:
-    print(f"{num} is an Even number.")
-else:
-    print(f"{num} is an Odd number.")
+try:
+    num = int(input("Enter a number: "))
+    if num % 2 == 0:
+        print(f"{num} is an Even number.")
+    else:
+        print(f"{num} is an Odd number.")
+except ValueError:
+    print("Please enter a valid integer !")
