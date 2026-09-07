e7074e7f66c72a5adebbb5df91f47ab29d5dc51e
Jeet Kumar <happyboy8117@gmail.com>
2026-09-07 18:52:36 +0530

    Create main1.py

---
+++ swapping of two numbers/main1.py
@@ -0,0 +1,13 @@
+try:
+  a = int(input("Enter the value of a : "))
+  b = int(input("Enter the value of b : "))
+  if a==b:
+    print("both are same numbers!")
+  else:
+    print(f"currently a = {a}\nb = {b}")
+    a+=b
+    b=a-b
+    a-=b
+    print(f"After swapping: a = {a}, b = {b}")
+except ValueError:
+  print("Enter valid integer !")
